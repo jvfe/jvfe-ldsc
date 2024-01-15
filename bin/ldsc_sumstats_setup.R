a88b7b4776c9ff95d42a60cf595d8e5b4706521d
Julia Apolonio <apoloniojulia@gmail.com>
2024-01-15 09:12:38 -0300

    fix: added info column to the output dataframe

--- bin/ldsc_sumstats_setup.R
+++ bin/ldsc_sumstats_setup.R
@@ -26,6 +26,6 @@ ref <- read_table(variants_file)  %>%
 # Perform the join and selection
 ldsc <- sumstats %>%
   inner_join(ref, by = "variant") %>%
-  dplyr::select(SNP=rsid, CHR=chr, A1=ref, A2=alt, P=pval, BETA=beta, N=n_complete_samples)
+  dplyr::select(SNP=rsid, CHR=chr, A1=ref, A2=alt, P=pval, BETA=beta, N=n_complete_samples, INFO=info)
 
 write_tsv(ldsc, output_name)
